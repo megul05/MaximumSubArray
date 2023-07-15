834b9b9e6968f8bada983cee4d94f09f2051c408
Megul Raj S <129952976+megul05@users.noreply.github.com>
2023-07-15 00:01:23 -0700

    Update MaximumSubArray.java

--- src/MaximumSubArray/MaximumSubArray.java
+++ src/MaximumSubArray/MaximumSubArray.java
@@ -1,9 +1,41 @@
 package MaximumSubArray;
 
+import java.util.*;
+
 public class MaximumSubArray {
+	
+	  public static int findMaxSub(int[] arr,int size,int m) {
+	        int sum = 0;
+	        int result = 0;
+	        for(int i=0;i<size;i++) {
+	            sum = 0;
+	            for(int j=0;j<size;j++)
+	            {
+	                if(sum+arr[j] <= m) 
+	                {
+	                    sum += arr[i];
+	                }
+	            }
+	            if(result<sum)
+	            {
+	                result = sum;
+	            }
+	        }
+	        return result;
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Size of the Array :");
+		int size = scan.nextInt();
+		System.out.println("Enter elements of the Array :");
+	        int[] arr = new int[size];
+	        for(int i=0;i<size;i++) {
+	            arr[i] = scan.nextInt();
+	        }
+	        int m = scan.nextInt();
+	        System.out.println(findMaxSub(arr, size, m));
 
 	}
 
